667c21bbf7d4e3de3ffc2514ff4c5ffa02b76eb4
Florin Diaconescu <florin.diaconescu@stud.acs.upb.ro>
2021-03-21 23:28:54 +0200

    Added main.c and smoltcp.h
    It provides a simple header for add_socket, add_socket_with_buffer (for custom
    rx and tx buffer size) and initing/destroying the stack. The return value from
    the Rust shim is good, and in case of adding a socket not yet supported (a dummy
    TCP and UDP implementation is provided for now), Rust panics and the program
    exits with the error message and a segmentation fault.
    
    Signed-off-by: Florin Diaconescu <florin.diaconescu@stud.acs.upb.ro>

--- main.c
+++ main.c
@@ -2,12 +2,24 @@
 /* Import user configuration: */
 #include <uk/config.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "smoltcp.h"
 
 int main(void)
 {
-	Stack s;
-	SmolSocket sock = add_socket(s, 0);
+    uint8_t ret;
+
+    smoltcp_stack_t *stack = init_stack();
+
+    ret = add_socket_with_buffer(stack, 0, 3500, 3500);
+    if (ret) {
+        printf("Wrong value! %d\n", ret);
+    }
+    else {
+        printf("Good value! %d\n", ret);
+    }
+
+    destroy_stack(stack);
 	return 0;
 }
--- smoltcp.h
+++ smoltcp.h
@@ -19,15 +19,21 @@ struct Ipv6AddressC {
     uint16_t ip_address[8];
 };
 
-typedef struct Stack {
-
-} Stack;
-
 typedef struct SmolSocket {
     uint8_t sockfd;
 } SmolSocket;
 
+typedef struct smoltcp_stack smoltcp_stack_t;
+
 // function definitions
-extern SmolSocket add_socket(Stack stack, uint8_t type);
+extern smoltcp_stack_t *init_stack(void);
+
+extern void destroy_stack(smoltcp_stack_t *stack);
+
+// add_socket with default rx_buffer and tx_buffer size
+extern uint8_t add_socket(smoltcp_stack_t *stack, uint8_t type);
+
+// add_socket with custom rx_buffer and tx_buffer size
+extern uint8_t add_socket_with_buffer(smoltcp_stack_t *stack, uint8_t type, uint8_t rx_buffer, uint8_t tx_buffer);
 
 #endif
